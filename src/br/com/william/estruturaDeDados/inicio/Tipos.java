02a75b15741d4c110092db6f7d9f89f4cabeeb67
John William Vicente <johnwill.v.2017@gmail.com>
2020-01-09 16:26:23 -0200

    Estudando arrays

---
+++ src/br/com/william/estruturaDeDados/inicio/Tipos.java
@@ -0,0 +1,22 @@
+package br.com.william.estruturaDeDados.inicio;
+public class Tipos{
+	public static void main(String args[]){
+		boolean verdadeiro=20>10;
+		char caractere='A';
+		byte b=12;
+		short s=24;
+		int i=257;
+		long l=890l;// Observar o uso do L aqui
+		float f=3.1415f; // Observar o uso do f
+		double d=2.1828;
+		System.out.println("verdade : "+verdadeiro);//'+' concatena strings
+		System.out.println("caractere : "+caractere);
+		System.out.println("b : "+b);
+		System.out.println("s : "+s);
+		System.out.println("i : "+i);
+		System.out.println("l : "+l);
+		System.out.println("f : "+f);
+		System.out.println("d : "+d);
+	}
+
+}
