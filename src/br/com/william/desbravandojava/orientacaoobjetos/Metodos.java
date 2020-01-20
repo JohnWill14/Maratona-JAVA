c3a67269ddaa4dceac8721cb22c8c1b24852ddc2
John William Vicente <johnwill.v.2017@gmail.com>
2020-01-20 16:02:12 -0200

    Metodos

---
+++ src/br/com/william/desbravandojava/orientacaoobjetos/Metodos.java
@@ -0,0 +1,40 @@
+package br.com.william.desbravandojava;
+
+public class Metodos{
+	/*
+		Metodos são como funcoes e procedimentos
+		mais são utilizados por objetos ou por classes, formato static.
+
+		modificador tipo interface(param){
+			body
+			return tipo;
+		} 
+
+		public abstract int soma(int a,int b); uso em classes abstrtas
+	*/
+	public String nome;
+	
+	public Metodos(){
+		//nao he permitido retorno
+	}
+	public Metodos(String nome){
+		this();
+		this.nome=nome;
+
+	}
+	public int soma(int a,int b){
+		return a+b;
+	}
+	public static void altera(Metodos met,String nome){
+		met.nome=nome;//met recebe a referencia do objeto
+	}
+	public static void main(String[] args) {
+		Metodos metodos=new Metodos();//como nao são static deve se instanciar o objeto
+		int soma=metodos.soma(1,2);
+		System.out.println(soma+"\n");
+
+		Metodos nome=new Metodos("john");
+		altera(nome,"joao");
+		System.out.println(nome.nome);
+	}
+}
---
+++ src/br/com/william/desbravandojava/orientacaoobjetos/Modifcadores.java
@@ -0,0 +1,33 @@
+package br.com.william.desbravandojava;
+
+public class Modifcadores{
+	/*
+	Modificadores:
+	
+		#public: deixa o atributo ou metodo no formato publico,
+		que pode ser acessado por QUALQUER classe.
+	
+		#private: deixa o atributo ou metodo no formato private,
+		que pode ser acessado APENAS pela sua classe.
+	
+		#protected: deixa o atributo ou metodo no formato protected,
+		que pode ser acessado por classes FILHAS ou de MESMO pacote.
+		
+
+		Se não especificado o atributo ou metodo se torna amigavel que 
+		em resumo só pode ser usado por classes do mesmo pacote. 
+		-Modificadores de uso:
+			#final: uma especie de constante public ex Math.PI.
+			#static: esse isola o acesso apenas para a classe ou seja
+			quando utilizo 'new' não é criada nem static pois ela já existe 
+			,por exemplo static int x=0; 'x' é utilizado apenas pela Classe
+			e não por objetos!
+	*/
+
+	public String nome;
+	private int id;
+	protected String filhas;
+	final float PI=3.1415f;
+
+
+}
