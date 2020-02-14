9df316779b8ce8c2b4c35ed465ea506fc6ac7e3f
John William Vicente <johnwill.v.2017@gmail.com>
2020-02-14 12:52:09 -0200

    Alterando /br/com/william/devdojo/Gassociacao/classe/Pessoa.java

--- src/br/com/william/devdojo/Gassociacao/classe/Pessoa.java
+++ src/br/com/william/devdojo/Gassociacao/classe/Pessoa.java
@@ -11,4 +11,30 @@ package br.com.william.devdojo.Gassociacao.classe;
  */
 public class Pessoa {
     private Departamento departamento;
+    private String nome;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Departamento departamento, String nome) {
+        this.departamento = departamento;
+        this.nome = nome;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
 }
